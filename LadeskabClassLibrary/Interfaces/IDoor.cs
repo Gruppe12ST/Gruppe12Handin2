9d1319ca8715ddfb27add29f4f60d1ce91aa2127
Sofie Bisgaard Hansen <201807880@post.au.dk>
2020-03-17 13:21:04 +0100

    Door og IDoor

--- LadeskabClassLibrary/Interfaces/IDoor.cs
+++ LadeskabClassLibrary/Interfaces/IDoor.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,9 +9,16 @@ namespace LadeskabClassLibrary
 {
     public class DoorOCEventArgs : EventArgs
     {
-
+        //Either open or closed
+        public bool Open { set; get; }
     }
     interface IDoor
     {
+        event EventHandler<DoorOCEventArgs> DoorOCEvent;
+        
+        bool DoorStatus { get; }
+
+        void LockDoor();
+        void UnlockDoor();
     }
 }
