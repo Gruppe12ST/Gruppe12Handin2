dc97b6afe876bbf4feee45477afac4896d99ab62
Christina <201807962@post.au.dk>
2020-03-25 13:58:24 +0100

    test af logfile i forbindelse med zombie

--- LadeskabUnitTest/LogFileUnitTest.cs
+++ LadeskabUnitTest/LogFileUnitTest.cs
@@ -27,11 +27,19 @@ namespace LadeskabUnitTest
             _uut = new LogFile(_dateTime);
         }
 
-
         [Test]
-        public void LogDoorLocked_LockedIdIs33_LockedDoorLogged()
+        public void zero_log()
         {
-            _uut.LogDoorLocked(33);
+            //Hvis der ikke er blevet logget, er der ikke kaldt til DatetimeProvider
+            _dateTime.DidNotReceive().GetDateTime();
+        }
+
+        //Logget en gang ved lås
+        [Test]
+        public void one_LogDoorLocked_LockedIdIs33_LockedDoorLogged()
+        {
+            int id = 33;
+            _uut.LogDoorLocked(id);
 
             _input = new FileStream(_path, FileMode.Open, FileAccess.Read);
             _fileReader = new StreamReader(_input);
@@ -41,17 +49,18 @@ namespace LadeskabUnitTest
             List<string> text = new List<string>();
             while ((inputR = _fileReader.ReadLine()) != null)
             {
-                inputF = inputR.Split(';');
-                text.Add(inputF[0]);
+                inputF = inputR.Split(':');
+                text.Add(inputF[3]);
             }
 
             _fileReader.Close();
 
-            string fiiletext = text.Last();
-            Assert.IsTrue(fiiletext.Length > 1);
+            int filid = Convert.ToInt32(text.Last());
+            Assert.That(id,Is.EqualTo(filid));
 
         }
 
+        //Logget en gang
         [Test]
         public void LogDoorLocked_LockedIdI33_datetimeprovider()
         {
@@ -59,11 +68,13 @@ namespace LadeskabUnitTest
             _dateTime.Received(1).GetDateTime();
         }
 
-
-        [Test]
-        public void LogDoorUnLocked_LockedIdIs34_UnLockedDoorLogged()
+        //Logget flere gange ved lås - tjek på id
+        [TestCase(45,23,87)]
+        public void LogDoorLocked_LockedFlereID_KorrektID(int id1, int id2, int id3)
         {
-            _uut.LogDoorUnlocked(34);
+            _uut.LogDoorLocked(id1);
+            _uut.LogDoorLocked(id2);
+            _uut.LogDoorLocked(id3);
 
             _input = new FileStream(_path, FileMode.Open, FileAccess.Read);
             _fileReader = new StreamReader(_input);
@@ -73,16 +84,56 @@ namespace LadeskabUnitTest
             List<string> text = new List<string>();
             while ((inputR = _fileReader.ReadLine()) != null)
             {
-                inputF = inputR.Split(';');
-                text.Add(inputF[0]);
+                inputF = inputR.Split(':');
+                text.Add(inputF[3]);
             }
 
             _fileReader.Close();
 
-            string fiiletext = text.Last();
-            Assert.IsTrue(fiiletext.Length > 1);
+            int filid = Convert.ToInt32(text.Last());
+            Assert.That(id3, Is.EqualTo(filid));
         }
 
+        //Logget flere gange 
+        [Test]
+        public void LogDoorLocked_LockIdI33_datatimeproviderRecived4()
+        {
+            _uut.LogDoorLocked(33);
+            _uut.LogDoorLocked(33); 
+            _uut.LogDoorLocked(33);
+            _uut.LogDoorLocked(33);
+
+            _dateTime.Received(4).GetDateTime();
+        }
+
+
+        //logget en gang ved oplåsning 
+        [Test]
+        public void LogDoorUnLocked_LockedIdIs34_UnLockedDoorLogged()
+        {
+            int id = 34;
+            _uut.LogDoorUnlocked(id);
+
+            _input = new FileStream(_path, FileMode.Open, FileAccess.Read);
+            _fileReader = new StreamReader(_input);
+
+            string inputR;
+            string[] inputF;
+            List<string> text = new List<string>();
+            while ((inputR = _fileReader.ReadLine()) != null)
+            {
+                inputF = inputR.Split(':'); 
+                text.Add(inputF[3]);
+                
+            }
+
+            _fileReader.Close();
+
+            int filid = Convert.ToInt32(text.Last());
+            Assert.That(id, Is.EqualTo(filid));
+        }
+
+        //Log en gang ved oplåsning
         [Test]
         public void LogDoorUnLocked_LockedIdI34_datetimeprovider()
         {
@@ -90,7 +141,44 @@ namespace LadeskabUnitTest
             _dateTime.Received(1).GetDateTime();
         }
 
-       
+        //Logget flere gange - tjek på id
+        [TestCase(45, 23, 87)]
+        public void LogDoorUnLocked_LockedFlereID_KorrektID(int id1, int id2, int id3)
+        {
+            _uut.LogDoorUnlocked(id1);
+            _uut.LogDoorUnlocked(id2);
+            _uut.LogDoorUnlocked(id3);
+
+            _input = new FileStream(_path, FileMode.Open, FileAccess.Read);
+            _fileReader = new StreamReader(_input);
+
+            string inputR;
+            string[] inputF;
+            List<string> text = new List<string>();
+            while ((inputR = _fileReader.ReadLine()) != null)
+            {
+                inputF = inputR.Split(':');
+                text.Add(inputF[3]);
+            }
+
+            _fileReader.Close();
+
+            int filid = Convert.ToInt32(text.Last());
+            Assert.That(id3, Is.EqualTo(filid));
+        }
+
+        //Logget flere gange 
+        [Test]
+        public void LogDoorUnLocked_LockIdI33_datatimeproviderRecived4()
+        {
+            _uut.LogDoorUnlocked(33);
+            _uut.LogDoorUnlocked(33);
+            _uut.LogDoorUnlocked(33); 
+            _uut.LogDoorUnlocked(33);
+
+            _dateTime.Received(4).GetDateTime();
+        }
+
 
     }
 }
